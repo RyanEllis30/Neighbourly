c2fb26bfd239e808ef628d8941b3aa7c428ec13e
Ryanellis30 <51757642+RyanEllis30@users.noreply.github.com>
2021-05-03 21:24:51 +0100

    Added database creation function

--- app/src/main/java/com/example/neighbourly/DatabaseHelper.kt
+++ app/src/main/java/com/example/neighbourly/DatabaseHelper.kt
@@ -16,13 +16,19 @@ class DatabaseHelper(context: Context):SQLiteOpenHelper(context, dbname, factory
     companion object {
         const val dbname = "Account"
         val factory = null
-        const val dbversion = 1
+        const val dbversion = 2
         var cursorCount = 0
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
         val createTable = "create table Account(ID_account integer primary key autoincrement, " +
-                "Email varchar(100), Password varchar(25)"
+                "Email varchar(100) not null, Password varchar(25) not null); create table Address(ID_address integer primary key autoincrement, " +
+                "HouseNum integer not null, Road varchar(50) not null, City varchar(50) not null, Postcode varchar(10) not null); create table User(ID_user integer primary key autoincrement, " +
+                "Name varchar(50) not null, Is_worker integer not null, Verified integer not null, Rating Real not null, foreign key Account_Id references Account(ID_account), " +
+                "foreign key Address_Id references Address(ID_address)); create table Task(ID_task integer primary key autoincrement, " +
+                "Description varchar(100) not null, Job_type varchar(50) not null, Status varchar(50) not null, Additional_requirements varchar(100) not null, " +
+                "foreign key Customer_Account_Id references Account(ID_account), foreign key Worker_Account_Id references Account(ID_account)); create table Review(ID_review integer primary key autoincrement, " +
+                "Rating integer not null, Description varchar(100) not null, foreign key Task_Id references Task(ID_task));"
         db?.execSQL("create table Account(ID_account integer primary key autoincrement, email varchar(100), password varchar(25))")
     }
 
