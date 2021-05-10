cac14569ba7708c4bce05be5aff2c31df09c54e7
ramanevos <78600795+ramanevos@users.noreply.github.com>
2021-05-11 00:56:53 +0100

    Added popup window to postjob

--- app/src/main/java/com/example/neighbourly/ui/postjob/PostJobFragment.kt
+++ app/src/main/java/com/example/neighbourly/ui/postjob/PostJobFragment.kt
@@ -1,21 +1,49 @@
 package com.example.neighbourly.ui.postjob
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.neighbourly.DatabaseHelper
 import com.example.neighbourly.R
 import java.util.*
 
 class PostJobFragment : Fragment() {
+
+    private lateinit var addjobDialog: AlertDialog
+    fun showCustomDialog() {
+        val inflater: LayoutInflater = this.getLayoutInflater()
+        val dialogView: View = inflater.inflate(R.layout.activity_newjob_popup, null)
+
+        //val header_txt = dialogView.findViewById<TextView>(R.id.ChangeAddress)
+        //header_txt.text = "Header Message"
+        //val details_txt = dialogView.findViewById<TextView>(R.id.housenumber)
+        val cancel: Button = dialogView.findViewById(R.id.Canceljob)
+        cancel.setOnClickListener {
+            addjobDialog.cancel()
+        }
+        val add: Button = dialogView.findViewById(R.id.addJob)
+        add.setOnClickListener {
+            // add code to add the job
+        }
+        val dialogBuilder: AlertDialog.Builder = AlertDialog.Builder(requireContext())
+        dialogBuilder.setOnDismissListener(object : DialogInterface.OnDismissListener {
+            override fun onDismiss(arg0: DialogInterface) {
+
+            }
+        })
+        dialogBuilder.setView(dialogView)
+
+        addjobDialog = dialogBuilder.create();
+        //alertDialog.window!!.getAttributes().windowAnimations = R.style.PauseDialogAnimation
+        addjobDialog.show()
+    }
 
     lateinit var handler: DatabaseHelper
 
@@ -26,24 +54,26 @@ class PostJobFragment : Fragment() {
     ): View? {
         handler = DatabaseHelper(requireContext())
 
+
         val root = inflater.inflate(R.layout.fragment_postjob, container, false)
 
         val postJobSubmitButton = root.findViewById<Button>(R.id.button2)
 
         postJobSubmitButton.setOnClickListener { // This is the submit button
-            val jobDescription = "jobDescription"
-            val jobType = "jobType"
-            val status = "status"
-            val additionalRequirements = "additionalRequirements"
-            val customerAccountId = "customerAccountId"
-            val workerAccountId = "workerAccountId"
+            showCustomDialog() }
+            //val jobDescription = "jobDescription"
+           // val jobType = "jobType"
+           // val status = "status"
+           // val additionalRequirements = "additionalRequirements"
+           // val customerAccountId = "customerAccountId"
+           // val workerAccountId = "workerAccountId"
 
-            if (this::handler.isInitialized) {
-                handler.createJob("$jobDescription", "$jobType", "$status", "$additionalRequirements", "$customerAccountId", "$workerAccountId")
-                Log.d("Errors", "Line 43 postjob")
-                Toast.makeText(requireContext(), "Creating Job", Toast.LENGTH_SHORT).show()
-            }
-        }
+           // if (this::handler.isInitialized) {
+           //     handler.createJob("$jobDescription", "$jobType", "$status", "$additionalRequirements", "$customerAccountId", "$workerAccountId")
+           //     Log.d("Errors", "Line 43 postjob")
+           //     Toast.makeText(requireContext(), "Creating Job", Toast.LENGTH_SHORT).show()
+
+
 
         return root
     }
